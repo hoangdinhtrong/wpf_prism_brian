94626782fa5ac28fead7b48d8d3f1b42252a8013
hoangdinhtrong <info.hoangdinhtrong.hy@gmail.com>
2022-05-22 22:27:49 +0700

    Delegate command

---
+++ WPF_Prism_Brian/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Prism.Ioc;
+using System.Windows;
+using WPF_Prism_Brian.Views;
+
+namespace WPF_Prism_Brian
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
